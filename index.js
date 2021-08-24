46a1f1899ba903cf883fb086c42391e66b3c815c
Rani1234-K <88437649+Rani1234-K@users.noreply.github.com>
2021-08-24 15:59:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let n1;
+console.log('value of n1 = ' + n1 + ' and data type of n1 = ' + n1);
+
+n1 = 100;
+console.log('value of n1 = ' + n1 + ' and data type of n1 =' + typeof(n1));
+
+n1 = "mahesh";
+console.log('value of n1 = ' + n1 + ' and data type of n1 =' + typeof(n1));
+
+n1 = "100.30";
+console.log('value of n1 = ' + n1 + ' and data type of n1 =' + typeof(n1));
+
+n1 = "true";
+console.log('value of n1 = ' + n1 + ' and data type of n1 =' + typeof(n1));
+
+n1 = "mahesh";
+console.log('value of n1 = ' + n1 + ' and data type of n1 =' + typeof(n1));
+
+n1 = [100,202,202];
+console.log('value of n1 = ' + n1 + ' and data type of n1 =' + typeof(n1));
+
+n1 = new Date(2021,3,10);
+console.log('value of n1 = ' + n1 + ' and data type of n1 =' + typeof(n1));
